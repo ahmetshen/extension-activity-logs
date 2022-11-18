371d2cc71d474c446fd52343f9bfff1bc52bbf35
ahmetshen <ahmetshen.jr@gmail.com>
2022-11-18 19:07:20 +0300

    Create Database/Migrations Files
    Update InstallCommand.php File

--- src/Console/InstallCommand.php
+++ src/Console/InstallCommand.php
@@ -4,6 +4,7 @@ namespace AhmetShen\ExtensionActivityLogs\Console;
 
 use Illuminate\Console\Command;
 use Illuminate\Filesystem\Filesystem;
+use Illuminate\Support\Str;
 
 class InstallCommand extends Command
 {
@@ -33,7 +34,7 @@ class InstallCommand extends Command
 
         // Database Files...
         $this->databaseFiles();
-        
+
         $this->info('extensionActivityLogs scaffolding installed successfully.');
 
         $this->comment('The installation process was carried out successfully. Please visit your web page.');
@@ -66,6 +67,18 @@ class InstallCommand extends Command
      */
     protected function migrationFiles(): void
     {
-        //
+        $migrationFiles = (new Filesystem)->allFiles(__DIR__.'/../../resources/stubs/database/migrations');
+
+        foreach ($migrationFiles as $migrationFile) {
+            $fileName = Str::beforeLast($migrationFile->getFilename(), '.stub');
+
+            if ((new Filesystem)->exists(database_path('migrations/'.$fileName.'.php'))) {
+                (new Filesystem)->delete(database_path('migrations/'.$fileName.'.php'));
+            }
+
+            (new Filesystem)->copy(__DIR__.'/../../resources/stubs/database/migrations/'.$fileName.'.stub', database_path('migrations/'.$fileName.'.php'));
+
+            unset($migrationFile);
+        }
     }
 }
